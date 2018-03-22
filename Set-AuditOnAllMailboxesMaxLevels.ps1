e3abbb86570d8d7e1c3aa97c2a05dbf35c960033
Peter Schmidt <peter@msdigest.net>
2018-03-22 12:50:22 +0100

    Update Set-AuditOnAllMailboxesMaxLevels.ps1

--- Set-AuditOnAllMailboxesMaxLevels.ps1
+++ Set-AuditOnAllMailboxesMaxLevels.ps1
@@ -1,12 +1,34 @@
-$GroupVar = DistributionGroupFromExchange
-Get-DistributionGroupMember $GroupVar | Get-MailboxPermission | where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false} | Select Identity,User,@{Name='Access Rights';Expression={[string]::join(', ', $_.AccessRights)}} | Export-Csv -NoTypeInformation .\DelegatedMailboxPermissions.csv
+<#
+    Author: Peter Schmidt (peter@msdigest.net)
+    Blog: www.msdigest.net
+    Notes:
+            This script will set the maximum level of Exchange Audit level, and
+            the run it against all Mailboxes in the environment.
 
-$Members=Get-DistributionGroupMember $GroupVar
- 
-foreach ($Member in $Members) { 
-    
-    Write-Host $Member
-    # Set the MailFrom, MailTo, MailServer and Days
-    # Download the Get-MailboxAuditLogging script from Practical365.com, credit by Paul Cunningham.
-    .\Get-MailboxAuditLogging.ps1 -Mailbox $Member -SendEmail -Hours 750 -MailFrom noreply@domain.com -MailTo mailuser@domain.com -MailServer localhost -Verbose
-     }
+            It needs to be run on one of the Exchange Servers.
+
+    To Retrieve a Report of the Audit, recommended this script by Paul Cunningham:
+            https://github.com/cunninghamp/Get-MailboxAuditLoggingReport.ps1
+
+    Version:
+            2018.03.03 - Inital version (PSC)
+#>
+#Logging
+#Start-Transcript 
+
+#Setting maximum Audit Level
+$AuditAdmin="Create,FolderBind,MessageBind,SendAs,SendOnBehalf,SoftDelete,HardDelete,Update,Move,Copy,MoveToDeletedItems"
+$AuditDelegate="Create,FolderBind,SendAs,SendOnBehalf,SoftDelete,HardDelete,Update,Move,MoveToDeletedItems"
+$AuditOwner="Create,SoftDelete,HardDelete,Update,Move,MoveToDeletedItems,MailboxLogin"
+
+#Set Audit Logging Days
+$AuditDays=365
+
+#Get all Mailboxes
+$MBX=Get-Mailbox -ResultSize unlimited
+
+#Setting the Audit Level for the Mailboxes
+$MBX | Set-Mailbox -AuditAdmin $AuditAdmin -AuditDelegate $AuditDelegate -AuditOwner $AuditOwner -AuditEnabled:$true -AuditLogAgeLimit $AuditDays
+
+#Stop Logging
+#Stop-Transcript
